49fd7e28f79f6a2be6a1b8c139f7a24524d6994e
Anupam kumari <anupamkumari362@gmail.com>
2021-02-01 10:36:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Landing from './component/Landing';
+
+function App() {
+  return (
+    <div className="App">
+    <h4>Level 2</h4>
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
